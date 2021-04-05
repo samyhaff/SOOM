d218218498df7560efdebf6276c7f20ea4492799
Samy Haffoudhi <haffoudhisamy@gmail.com>
2021-04-05 18:00:29 +0200

    added borders

--- draw.cpp
+++ draw.cpp
@@ -3,20 +3,20 @@
 void draw(game *game)
 {
     // clear the screen
-    SDL_SetRenderDrawColor(game->renderer, 0, 0, 0, 255);
+    SDL_SetRenderDrawColor(game->renderer, 128, 128, 128, 255);
     SDL_RenderClear(game->renderer);
 
     // draw the map
-    SDL_SetRenderDrawColor(game->renderer, 255, 255, 255, 255);
     for (int i = 0; i < game->board.h; i++)
     {
         for (int j = 0; j <= game->board.w; j++)
         {
             if (game->board.arr[i][j] == WALL)
-            {
-                SDL_Rect cell_rect = {j * game->board.xStep, i * game->board.yStep, game->board.xStep, game->board.yStep};
-                SDL_RenderFillRect(game->renderer, &cell_rect);
-            }
+                SDL_SetRenderDrawColor(game->renderer, 255, 255, 255, 255);
+            else 
+                SDL_SetRenderDrawColor(game->renderer, 0, 0, 0, 255);
+            SDL_Rect cell_rect = {j * game->board.xStep, i * game->board.yStep, game->board.xStep - 2, game->board.yStep - 2};
+            SDL_RenderFillRect(game->renderer, &cell_rect);
         }
     }
 
